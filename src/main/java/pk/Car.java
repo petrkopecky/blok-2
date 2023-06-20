9611fac279ccfadae03f2d1d2c73f227076382f6
Kopecký Petr <petr.kopecky@t-mobile.cz>
2023-06-20 15:12:00 +0200

    car1980

--- src/main/java/pk/Car.java
+++ src/main/java/pk/Car.java
@@ -1,2 +1,46 @@
-package pk;public class Car {
+package pk;
+
+public class Car {
+private String make;
+private String model;
+private Integer year;
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public Car(String make, String model, Integer year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "make='" + make + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                '}';
+    }
 }
