4b056d232341b691acef4c5714e3f2b1d1671134
Kopecký Petr <petr.kopecky@t-mobile.cz>
2023-06-20 15:28:24 +0200

    rectangle area1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.example.pk.skoleni.Main20230620a;
 import pk.Car;
 import pk.Rectangle;
 
@@ -7,14 +8,7 @@ import pk.Rectangle;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        Car car1 = new Car("trabant", "combi", 1960);
-        System.out.println(car1.toString());
-        car1.setYear(1980);
-        System.out.println(car1.toString());
-
-        Rectangle rectangle = new Rectangle(1, 2);
-        double rectangleArea = rectangle.getArea();
-        System.out.println("area:" + rectangleArea);
+        Main20230620a.main(args);
 
 
     }
