e8e244b9138c8767fc8c806b0938ab4b7b6b4cf6
Kopecký Petr <petr.kopecky@t-mobile.cz>
2023-06-20 15:21:53 +0200

    rectangle area

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,21 @@
 package org.example;
 
 import pk.Car;
+import pk.Rectangle;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-       Car car1=new Car("trabant","combi",1960);
-       System.out.println(car1.toString());
-       car1.setYear(1980);
-       System.out.println(car1.toString());
+        Car car1 = new Car("trabant", "combi", 1960);
+        System.out.println(car1.toString());
+        car1.setYear(1980);
+        System.out.println(car1.toString());
+
+        Rectangle rectangle = new Rectangle(1, 2);
+        double rectangleArea = rectangle.getArea();
+        System.out.println("area:" + rectangleArea);
+
 
     }
 }
