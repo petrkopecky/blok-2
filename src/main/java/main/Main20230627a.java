0ba07313a5093f244e0cb99ec1894460378055e3
Kopecký Petr <petr.kopecky@t-mobile.cz>
2023-06-27 14:30:40 +0200

    compos

---
+++ src/main/java/main/Main20230627a.java
@@ -0,0 +1,20 @@
+package main;
+
+import pk.Car;
+import pk.Rectangle;
+
+
+public class Main20230620a {
+    public static void main(String[] args) {
+        Car car1 = new Car("trabant", "combi", 1960);
+        System.out.println(car1.toString());
+        car1.setYear(1980);
+        System.out.println(car1.toString());
+
+        Rectangle rectangle = new Rectangle(1, 2);
+        double rectangleArea = rectangle.getArea();
+        System.out.println("area:" + rectangleArea);
+
+
+    }
+}
